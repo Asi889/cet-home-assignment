498c6290c0c46e83eddb4601bc29e8c2501a7494
asi marom <asafmarom89@gmail.com>
2024-04-09 19:18:32 +0300

    getAllTasks fix

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getAllTasks } from "./../../actions/ticketActions";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET() {
   const allTickets = await getAllTasks();
   return new Response(JSON.stringify(allTickets), { status: 200 });
 }
